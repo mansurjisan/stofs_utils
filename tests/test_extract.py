dea1ad6b5260bd3f05578df5bb47688b256d49b7
mansurjisan <mjisan@DESKTOP-LM61H41>
2025-04-13 11:26:16 -0400

    Fix: Avoid shadowing numpy alias 'np' in test_extract.py

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -6,12 +6,14 @@ from stofs_utils.processing.extract import extract_2d_slab, write_slab_netcdf
 
 def test_extract_2d_slab():
     # Create synthetic data
-    time, nvrt, np = 3, 5, 4
-    data = np.random.rand(time, nvrt, np)
+    # time, nvrt, np = 3, 5, 4
+    # data = np.random.rand(time, nvrt, np)
+    time_steps, nvrt, n_nodes = 3, 5, 4
 
     # Fake weights (interpolating between level 2 and 3)
-    k0 = np.full((time, np), 2)
-    alpha = np.random.rand(time, np)
+    data = np.random.rand(time_steps, nvrt, n_nodes)
+    k0 = np.full((time_steps, n_nodes), 2)
+    alpha = np.random.rand(time_steps, n_nodes)
 
     # Create a mock xarray Dataset
     ds = xr.Dataset({
@@ -22,17 +24,18 @@ def test_extract_2d_slab():
     slab = extract_2d_slab(ds, 'temp', k0, alpha)
 
     # Check dimensions
-    assert slab.shape == (time, np)
+    # assert slab.shape == (time, np)
+    assert slab.shape == (time_steps, n_nodes) 
     assert not np.any(np.isnan(slab))
 
 
 def test_write_slab_netcdf():
     # Dummy data
-    nt, np = 3, 4
-    time = np.arange(nt)
-    x = np.linspace(-70, -69, np)
-    y = np.linspace(40, 41, np)
-    slab = np.random.rand(nt, np)
+    nt, n_nodes = 3, 4 # Use n_nodes instead of np
+    time_vals = np.arange(nt) # Use np (NumPy) correctly here
+    x = np.linspace(-70, -69, n_nodes)
+    y = np.linspace(40, 41, n_nodes)
+    slab = np.random.rand(nt, n_nodes)
     varname = "temperature"
     attrs = {
         "long_name": "Sea temperature",
@@ -47,5 +50,5 @@ def test_write_slab_netcdf():
         # Reopen and verify
         with Dataset(tmp.name) as ds:
             assert varname in ds.variables
-            assert ds.variables[varname].shape == (nt, np)
-            assert np.allclose(ds.variables['time'][:], time)
+            assert ds.variables[varname].shape == (nt, n_nodes) 
+            assert np.allclose(ds.variables['time'][:], time_vals)
